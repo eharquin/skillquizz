1d0884a71aeff4d608813627fcc831a567a8e25c
Lidia Blondel <lidia.blondel@etu.utc.fr>
2022-10-04 08:55:40 +0000

    Update QuestionController.java

--- src/main/java/fr/utc/skillquizz/controllers/QuestionController.java
+++ src/main/java/fr/utc/skillquizz/controllers/QuestionController.java
@@ -4,6 +4,7 @@ import fr.utc.skillquizz.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class QuestionController {
 
     @Autowired

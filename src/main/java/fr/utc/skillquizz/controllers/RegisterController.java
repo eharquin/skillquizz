04a8398b9974da4cb58cb41974ab28b79b6c5f36
Antonin <antonin.guyot@icloud.com>
2022-10-04 11:44:01 +0200

    DTO

--- src/main/java/fr/utc/skillquizz/controllers/RegisterController.java
+++ src/main/java/fr/utc/skillquizz/controllers/RegisterController.java
@@ -1,7 +1,13 @@
 package fr.utc.skillquizz.controllers;
 
+import fr.utc.skillquizz.dto.UserDto;
+import fr.utc.skillquizz.models.User;
 import fr.utc.skillquizz.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +16,19 @@ public class RegisterController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
 
+    @PostMapping(path = "/register", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> register(@RequestBody UserDto userDto){
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setName(userDto.getName());
+        user.setCompany(userDto.getCompany());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setPassword(userDto.getPassword());
+        userRepository.save(user);
+
+        return new ResponseEntity<>(modelMapper.map(user, UserDto.class), HttpStatus.OK);
+    }
 }
--- src/main/java/fr/utc/skillquizz/repositories/UserRepository.java
+++ src/main/java/fr/utc/skillquizz/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package fr.utc.skillquizz.repositories;
 import fr.utc.skillquizz.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }

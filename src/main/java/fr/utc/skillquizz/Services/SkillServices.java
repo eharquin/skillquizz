42a1010e1f48766958475ed8a592617d0cfe5dc6
Benjamin <benjamin@rigitano.com>
2022-10-04 11:50:49 +0200

    skillService

---
+++ src/main/java/fr/utc/skillquizz/Services/SkillServices.java
@@ -0,0 +1,32 @@
+package fr.utc.skillquizz.Services;
+
+import fr.utc.skillquizz.models.Skill;
+import fr.utc.skillquizz.repositories.SkillRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+@Transactional
+public class SkillServices {
+
+    @Autowired
+    private final SkillRepository skillRepository;
+
+    public SkillServices(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
+
+    public void deleteSkill(Long skillId){
+        this.skillRepository.findById(skillId);
+        this.skillRepository.deleteAnswerById(skillId);
+    }
+
+    public Skill createSkill(Skill skill){
+        System.out.println("Skill : " + skill.getSubject());
+        return this.skillRepository.save(skill);
+    }
+}

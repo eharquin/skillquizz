35df79af295ba0cd15272fc555dde62569ca2874
Antonin <antonin.guyot@icloud.com>
2022-10-04 10:56:25 +0200

    Fix

--- src/main/java/fr/utc/skillquizz/controllers/QuestionController.java
+++ src/main/java/fr/utc/skillquizz/controllers/QuestionController.java
@@ -1,5 +1,6 @@
 package fr.utc.skillquizz.controllers;
 
+import fr.utc.skillquizz.models.Question;
 import fr.utc.skillquizz.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +12,27 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @GetMapping("/question")
-    public void index(){
+    public void index() {
 
     }
 
     @GetMapping("/question/{id}")
-    public void show(@PathVariable int questionId){
+    public void show(@PathVariable int questionId) {
 
     }
 
     @PostMapping("/question")
-    public void store(@RequestBody Question question){
+    public void store(@RequestBody Question question) {
 
     }
 
     @PatchMapping("/question/{id}")
-    public void update(@PathVariable int questionId, @RequestBody Question question){
+    public void update(@PathVariable int questionId, @RequestBody Question question) {
 
     }
 
     @DeleteMapping("/question/{id}")
-    public void destroy(@PathVariable int questionId){
+    public void destroy(@PathVariable int questionId) {
 
     }
+}

cfaaaf0707c480d1d3473ae43eb5d9cfa00eda1f
Marine Marsal <marine.marsal@etu.utc.fr>
2022-12-12 17:25:04 +0100

    correction annotation

--- src/main/java/fr/utc/skillquizz/repositories/QuestionRepository.java
+++ src/main/java/fr/utc/skillquizz/repositories/QuestionRepository.java
@@ -2,10 +2,11 @@ package fr.utc.skillquizz.repositories;
 
 import fr.utc.skillquizz.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> deleteAnswerById(Long id);

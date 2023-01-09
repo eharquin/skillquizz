a945c9ade0bd4e42a4324ac9770df6ce47cb9577
Antonin <antonin.guyot@icloud.com>
2023-01-09 17:36:29 +0100

    Search and pagination

--- src/main/java/fr/utc/skillquizz/repositories/QuizzRepository.java
+++ src/main/java/fr/utc/skillquizz/repositories/QuizzRepository.java
@@ -1,12 +1,16 @@
 package fr.utc.skillquizz.repositories;
 
-import fr.utc.skillquizz.models.Answer;
 import fr.utc.skillquizz.models.Quizz;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
+
 @Repository
 public interface QuizzRepository extends JpaRepository<Quizz, Long> {
+    Page<Quizz> searchBySkillContaining(String skill, Pageable pageable);
 
+    Page<Quizz> findAll(Pageable pageable);
 }
